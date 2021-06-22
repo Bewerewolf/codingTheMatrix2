71174e5ed5e26513d7b0ebeee9f2932c48d92fbc
Kirk Haller <kirkhaller@gmail.com>
2021-06-22 19:33:24 -0400

    Added a dictionary

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main():
     print("hola")
+    # This is a dictionary
+    my_dictionary = dict()
+    my_dictionary["first"] = "Kirk"
+    print(my_dictionary)
+
 
 if __name__ == '__main__':
     main()
