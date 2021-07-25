85a916623918c4684f72b95815e24dc5fc400f97
Bewerewolf <erik.haller@me.com>
2021-07-25 14:27:54 -0400

    identity matrix ig

--- matrixShtuff.py
+++ matrixShtuff.py
@@ -1,25 +1,32 @@
 import numpy as np
+import copy
 
 def main():
     A = [[2, 2, 3], [4, 5, 6], [2, 3, 4]]
     print(A)
     print(solve_matrix(A))
+    print(A)
 
 def solve_matrix(A):
-    V = A
-    print(V[0])
-    for index in range(len(V[0])):
-        value = V[index][index]
+    V = copy.deepcopy(A)
+    #print(V[0])
+    for row in range(len(V[0])):
+        value = V[row][row]
         # Dividing by the first non-zero element
-        for jindex in range(index, len(V[index])):
-            V[index][jindex] = V[index][jindex] / value
+        for column in range(row, len(V[row])):
+            V[row][column] = V[row][column] / value
 
         # Elimination
-        if index > 0 and index < V[index]:
-            new_val = V[index + 1][index]
-        for jindex in range(index+1, len(V[index])):
-            print('index: ', index, ' jindex: ', jindex, ' new_val: ', new_val)
-            V[index+1][index] = V[index+1][index] - new_val * V[index][index]
+        #for jindex in range(index, len(V[index])-1):
+           # new_val = V[index + 1][index]
+            #print(new_val)
+        for row2 in range(row+1, len(V[row])):
+            new_val = V[row2][row]
+            #print("new value = ", new_val)
+            #print('index: ', index, ' jindex: ', jindex, ' new_val: ', new_val)
+            for column in range(row, len(V[row])):
+                V[row2][column] = V[row2][column] - new_val * V[row][column]
+            #print(V[row2])
 
     return V
 
