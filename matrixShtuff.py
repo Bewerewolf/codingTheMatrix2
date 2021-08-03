89543a00f3d07f75b2c7cb5ccb13acaa59a2fa1e
Bewerewolf <erik.haller@me.com>
2021-08-03 14:21:07 -0400

    fixing blasphemy

--- matrixShtuff.py
+++ matrixShtuff.py
@@ -2,31 +2,22 @@ import numpy as np
 import copy
 
 def main():
-    A = [[2, 2, 3], [4, 5, 6], [2, 3, 4]]
-    print(A)
-    print(solve_matrix(A))
-    print(A)
+    A = [[2.0, 2.0, 3.0], [4.0, 5.0, 6.0], [2.0, 3.0, 4.0]]
+    numpA = np.array(A)
+    print(numpA)
+    print(solve_matrix(numpA))
+    print(numpA)
 
 def solve_matrix(A):
     V = copy.deepcopy(A)
-    #print(V[0])
+    print(V[0])
     for row in range(len(V[0])):
-        value = V[row][row]
-        # Dividing by the first non-zero element
-        for column in range(row, len(V[row])):
-            V[row][column] = V[row][column] / value
+        V[row] = V[row]/V[row][row]
 
         # Elimination
-        #for jindex in range(index, len(V[index])-1):
-           # new_val = V[index + 1][index]
-            #print(new_val)
         for row2 in range(row+1, len(V[row])):
             new_val = V[row2][row]
-            #print("new value = ", new_val)
-            #print('index: ', index, ' jindex: ', jindex, ' new_val: ', new_val)
-            for column in range(row, len(V[row])):
-                V[row2][column] = V[row2][column] - new_val * V[row][column]
-            #print(V[row2])
+            V[row2] = V[row2] - new_val*V[row]
 
     return V
 
